93a1ee8a9d094190ec2e194ddcac3d86dd189876
Amanda Katariny <amandakata@outlook.com>
2021-11-30 10:21:34 -0300

    firt commit

---
+++ src/hospital_test_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns hospital-test-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/hospital_test_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns hospital-test-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [hospital-test-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
